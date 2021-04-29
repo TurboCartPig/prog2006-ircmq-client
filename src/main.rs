18352054fddb6ddd943b6cbcc7445fff1aff5fea
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-04-29 18:24:03 +0200

    Add basic chat and serde

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
+use serde::{Deserialize, Serialize};
+
+/// Messages to be serialized and sent to the server.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum MessageType {
+    /// Initial message sent when a client first connects.
+    Hello { name: String },
+    /// A text message sent from a client to all the other clients, through the server.
+    Message { content: String },
+    /// Final message from client to server, notifying the server, that the client is leaving.
+    Goodbye,
+}
+
 fn main() {
     let context = zmq::Context::new();
     let server = std::env::args().nth(1);
 
     if server.is_some() {
-        let socket = context.socket(zmq::REP).unwrap();
-        socket.bind("tcp://*:5555").unwrap();
+        let rep_socket = context.socket(zmq::REP).unwrap();
+        rep_socket.bind("tcp://*:5555").unwrap();
+
+        let pub_socket = context.socket(zmq::PUB).unwrap();
+        pub_socket.bind("tcp://*:6666").unwrap();
 
         let mut msg = zmq::Message::new();
 
         loop {
-            socket.recv(&mut msg, 0).unwrap();
-            socket.send(msg.as_str().unwrap(), 0).unwrap();
+            rep_socket.recv(&mut msg, 0).unwrap();
+            println!("Received: {}", msg.as_str().unwrap());
+            rep_socket.send("ACK", 0).unwrap();
+            pub_socket.send("A", zmq::SNDMORE).unwrap();
+            pub_socket.send(msg.as_str().unwrap(), 0).unwrap();
         }
     } else {
-        let socket = context.socket(zmq::REQ).unwrap();
-        socket.connect("tcp://localhost:5555").unwrap();
+        let req_socket = context.socket(zmq::REQ).unwrap();
+        req_socket.connect("tcp://localhost:5555").unwrap();
+
+        let sub_socket = context.socket(zmq::SUB).unwrap();
+        sub_socket.set_subscribe(b"A").unwrap();
+        sub_socket.connect("tcp://localhost:6666").unwrap();
 
         let mut msg = zmq::Message::new();
 
         for n in 0..10 {
-            socket.send(&format!("Hello {}", n), 0).unwrap();
-            socket.recv(&mut msg, 0).unwrap();
+            req_socket.send(&format!("Hello {}", n), 0).unwrap();
+            req_socket.recv(&mut msg, 0).unwrap();
+            sub_socket.recv_string(0).unwrap().unwrap();
+            sub_socket.recv(&mut msg, 0).unwrap();
             println!("Received: {}", msg.as_str().unwrap());
+            std::thread::sleep(std::time::Duration::from_secs(1u64));
         }
     }
 }
